4aaecc738d1495ec6327686a71e966f08ab0a9ec
Andre Rodriguez Sabino <andresabinocalvo@gmail.com>
2022-08-21 20:12:56 +0100

    Delete Game System backup v0.4 directory

--- Game System backup v0.4/footer.php
+++
@@ -1,6 +0,0 @@
-<?php
-echo "<footer style='text-align: center';>";
-echo "<p>Accessed by " . $_SERVER['REMOTE_ADDR'] . " on " . date('d/M/Y') . "</p>";
-echo "<p>Developed by Andre Rodriguez Sabino &copy; 2022</p>";
-echo "</footer>";
-$data->close();
